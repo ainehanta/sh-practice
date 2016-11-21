663e63001059e5d0c4a4d8c31767dfee8dd3299c
user03 <user03@ex03.local.j.kisarazu.ac.jp>
2016-11-21 09:43:51 +0900

    fix relative path

--- rv.sh
+++ rv.sh
@@ -3,7 +3,10 @@
 # create the trash can if doesn't exist.
 mkdir -p "${HOME}/.trash"
 
+current_path=$(pwd)
+
 for item in $@; do
+    cd $current_path
     cd $(dirname $item)
     converted_path=$(pwd | sed -e 's/\//%/g')
     uniq_id=$(od -vAn -N4 -tu4 < /dev/urandom | tr -d ' ')
